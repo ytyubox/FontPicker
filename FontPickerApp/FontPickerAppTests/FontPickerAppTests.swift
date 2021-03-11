60f554bd55400eace9adb4e7cab82a576e8060b7
Tsungyu Yu <ytyubox@gmail.com>
2021-03-11 16:45:17 +0800

    add API KEY testing

--- FontPickerApp/FontPickerAppTests/FontPickerAppTests.swift
+++ FontPickerApp/FontPickerAppTests/FontPickerAppTests.swift
@@ -15,14 +15,23 @@ class FontPickerAppTests: XCTestCase {
     
     
     
-    func testMainBundleHaveAPIKEY() throws {
+    override func setUpWithError() throws {
+        try guardMainbundleHaveAPIKEYElseSkip()
+    }
+    func test() {
+        XCTFail()
+    }
+    
+    fileprivate func guardMainbundleHaveAPIKEYElseSkip(file: StaticString = #file, line: UInt = #line) throws {
         let bundle = Bundle.main
         let dictory = try XCTUnwrap(bundle.infoDictionary)
         let valueFromKey = dictory[APIKEY_PLISTKEY]
         let value = try XCTUnwrap(valueFromKey)
         let key = try XCTUnwrap(value as? String)
-        XCTAssertFalse(key.isEmpty)
+        if key.isEmpty {
+            throw XCTSkip("API key is empty, set the key in secrets.xccongfig file",file: file, line: line)
+        }
     }
-    private let APIKEY_PLISTKEY = "GOOGLEAPIKEY"
 }
 
+private let APIKEY_PLISTKEY = "GOOGLEAPIKEY"
