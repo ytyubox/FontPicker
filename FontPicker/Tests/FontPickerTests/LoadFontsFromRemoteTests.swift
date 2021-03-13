d1bda92175ef2d78eeeeb2c8f4c2e84b890f4759
Tsungyu Yu <ytyubox@gmail.com>
2021-03-13 11:29:01 +0800

    Spec: loader being deallocated will not devliver result to client

--- FontPicker/Tests/FontPickerTests/LoadFontsFromRemoteTests.swift
+++ FontPicker/Tests/FontPickerTests/LoadFontsFromRemoteTests.swift
@@ -127,6 +127,19 @@ final class LoadFontsFromRemoteTests: XCTestCase {
             client.complete(withStatusCode: 200, data: json)
         })
     }
+    func test_load_doesNotDeliverResultAfterSUTInstanceHasBeenDeallocated() {
+        let url = URL(string: "http://any-url.com")!
+        let client = HTTPClientSpy()
+        var sut: SUT? = RemoteFontLoader(url: url, client: client)
+
+        var capturedResults = [SUT.Outcome]()
+        sut?.load { capturedResults.append($0) }
+
+        sut = nil
+        client.complete(withStatusCode: 200, data: makeItemsJSON([]))
+
+        XCTAssertTrue(capturedResults.isEmpty)
+    }
 
 
     // MARK: - Helper
