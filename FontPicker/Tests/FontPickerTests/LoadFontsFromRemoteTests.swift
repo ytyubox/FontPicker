9f801941b4f663a7120c3618c239083cf35e5ebf
Tsungyu Yu <ytyubox@gmail.com>
2021-03-13 10:38:08 +0800

    spec: load will load the url

--- FontPicker/Tests/FontPickerTests/LoadFontsFromRemoteTests.swift
+++ FontPicker/Tests/FontPickerTests/LoadFontsFromRemoteTests.swift
@@ -19,13 +19,42 @@ final class LoadFontsFromRemoteTests: XCTestCase {
         XCTAssertTrue(client.requestedURLs.isEmpty)
     }
 
+    func test_load_requestsDataFromURL() {
+        let url = URL(string: "https://a-given-url.com")!
+        let (sut, client) = makeSUT(url: url)
+
+        sut.load { _ in }
+
+        XCTAssertEqual(client.requestedURLs, [url])
+    }
+
+    func test_loadTwice_requestsDataFromURLTwice() {
+        let url = URL(string: "https://a-given-url.com")!
+        let (sut, client) = makeSUT(url: url)
+
+        sut.load { _ in }
+        sut.load { _ in }
+
+        XCTAssertEqual(client.requestedURLs, [url, url])
+    }
+
     // MARK: - Helper
+
+    private typealias SUT = RemoteFontLoader
+    private func makeSUT(url: URL = URL(string: "https://a-url.com")!, file: StaticString = #filePath, line: UInt = #line) -> (sut: SUT, client: HTTPClientSpy) {
+        let client = HTTPClientSpy()
+        let sut = RemoteFontLoader(url: url, client: client)
+        trackForMemoryLeaks(sut, file: file, line: line)
+        trackForMemoryLeaks(client, file: file, line: line)
+        return (sut, client)
+    }
 
     private final class HTTPClientSpy: HTTPClient {
         private(set) var requestedURLs = [URL]()
 
-        func get(from _: URL, completion _: @escaping (HTTPClient.Result) -> Void) -> HTTPClientTask {
-            Task()
+        func get(from url: URL, completion _: @escaping (HTTPClient.Result) -> Void) -> HTTPClientTask {
+            requestedURLs.append(url)
+            return Task()
         }
 
         private struct Task: HTTPClientTask {
